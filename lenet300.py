b7a163fed7b68dcb0d06097cf4bbdb2737a6dd75
akt <47710249+Arnab9Codes@users.noreply.github.com>
2023-10-09 13:23:14 -0400

    Update lenet300.py

--- lenet300.py
+++ lenet300.py
@@ -29,18 +29,23 @@ class lenet_300(nn.Module):
         super(lenet_300, self).__init__()
         self.fc1 = nn.Linear(28 * 28, 300)
         self.relu1 = nn.ReLU(inplace = True)
+    
         self.relu2 = nn.ReLU(inplace = True)
+    
         self.fc3 = nn.Linear(100, 10)
-        self.weight = nn.Parameter(torch.Tensor(300, 100))
-        self.bias = nn.Parameter(torch.Tensor(100)) 
+
+        self.weight = nn.Parameter(torch.Tensor(300, 100), requires_grad=True)
+        self.bias = nn.Parameter(torch.Tensor(100), requires_grad=True) 
+        self.init_params = self.state_dict()
     
     def forward(self, x):
         x = x.view(x.size(0), 28 * 28)
-        out = self.fc1(x)
+        out = self.fc1(x) 
         out = self.relu1(out)
+        #print(out.shape)
         out = linear_new(out, self.weight, self.bias)
         out = self.relu2(out)
-        logits = self.fc3(out)# nn.Softmax(out) 
+        logits = self.fc3(out)
     
         return logits 
 
@@ -63,8 +68,26 @@ for epoch in range(num_epochs):
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad() 
         outputs = lenet_model(inputs)
+        #print('outputs')
         loss = criterion(outputs, targets)
+
         loss.backward()
         optimizer.step()
         
         loss_total+= loss.item()
+    print(loss.item())
+
+lenet_model.eval()  
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for inputs, targets in test_loader:
+        inputs, targets = inputs.to(device), targets.to(device)
+        outputs = lenet_model(inputs)
+        _, predicted = torch.max(outputs, 1)
+        total += targets.size(0)
+        correct += (predicted == targets).sum().item()
+
+acc = 100 * correct / total
+print(f'Test Accuracy: {acc:.f}%')
